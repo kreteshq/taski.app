e141be096ed1e295e2dbfb5174825db69b02a461
Zaiste <oh@zaiste.net>
2021-05-22 23:12:57 +0200

    fix tailwind generation

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,5 +1,18 @@
+
 module.exports = {
-  theme: {},
+  mode: 'jit',
+  purge: [
+    './site/**/*.{js,jsx,ts,tsx,vue}',
+    './app/components/**/*.{js,jsx,ts,tsx,vue}',
+    './safelist.css.txt'
+  ],
+  theme: {
+    extend: {
+    },
+  },
   variants: {},
-  plugins: [],
+  plugins: [
+  ],
 }
+
+
