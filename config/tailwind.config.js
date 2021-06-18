571a0e8b7e322ddc729115f1fdbf1e13174e34f1
Zaiste <oh@zaiste.net>
2021-06-18 17:35:44 +0200

    move from `app/` to `client/` & `server/`

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -3,7 +3,7 @@ module.exports = {
   mode: 'jit',
   purge: [
     './site/**/*.{js,jsx,ts,tsx,vue}',
-    './app/components/**/*.{js,jsx,ts,tsx,vue}',
+    './client/components/**/*.{js,jsx,ts,tsx,vue}',
     './safelist.css.txt'
   ],
   theme: {
